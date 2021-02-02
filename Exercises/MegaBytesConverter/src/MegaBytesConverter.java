248690ee8fcb65855fc139e4c6af73647da4142e
Daniel Tujo <danieltujo@lewisu.edu>
2021-02-01 23:25:59 -0600

    Final

--- Exercises/MegaBytesConverter/src/MegaBytesConverter.java
+++ Exercises/MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,7 +1,23 @@
 public class MegaBytesConverter {
 
 
+    public static void main(String[] args) {
+        int kiloBytes = 2500;
+        printMegaBytesAndKiloBytes(kiloBytes);
+
+    }
+
+
+
     public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int kiloRemainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + kiloRemainder + " KB" );
+        }
+
 
     }
 }
