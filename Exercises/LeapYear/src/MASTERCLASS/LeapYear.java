a9dbb71c7e460a75dcfa611b17e595ee4fbd435d
Daniel Tujo <danieltujo@lewisu.edu>
2021-02-01 23:59:11 -0600

    First and final

---
+++ Exercises/LeapYear/src/MASTERCLASS/LeapYear.java
@@ -0,0 +1,18 @@
+package MASTERCLASS;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1924));
+    }
+
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if ((year % 4 == 0) && (year % 100 != 0)) {
+            return true;
+        } else return (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0);
+
+    }
+}
