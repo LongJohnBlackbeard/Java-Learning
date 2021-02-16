94bb6cce5799162e969a0971058937be988317bf
Daniel <danieltujo@lewisu.edu>
2021-02-16 07:55:26 -0600

    done

--- Notes/TheForStatement/src/Main.java
+++ Notes/TheForStatement/src/Main.java
@@ -9,22 +9,51 @@ public class Main {
 
 
 //         (init; terminate; interval)
-        for (int i = 2; i < 9; i++) {
-            System.out.println("10,000 at " + i + "% interest = " +
-                    String.format("%.2f", calculateInterest(10000.0, i)));
-        }
+//        for (int i = 2; i < 9; i++) {
+//            System.out.println("10,000 at " + i + "% interest = " +
+//                    String.format("%.2f", calculateInterest(10000.0, i)));
+//        }
+//
+//        System.out.println("**********************************");
+//
+//        for(int i=8; i>=2; i--) {
+//            System.out.println("10,000 at " + i + "% interest = " +
+//                    String.format("%.2f", calculateInterest(10000.0, i)));
+//        }
 
-        System.out.println("**********************************");
+        int primeNumbers = 0;
+        for(int i= 10; i < 50; i++){
 
-        for(int i=8; i>=2; i--) {
-            System.out.println("10,000 at " + i + "% interest = " +
-                    String.format("%.2f", calculateInterest(10000.0, i)));
+            if (isPrime(i)) {
+                primeNumbers ++;
+                System.out.println(i + " is a prime number and we have found " + primeNumbers + " prime numbers");
+                if (primeNumbers == 3) {
+                    break;
+                }
+            }
         }
     }
 
-    public static double calculateInterest(double amount, double interestRate) {
-        return (amount * (interestRate / 100));
+
+
+    public static boolean isPrime(int n) {
+
+        if(n == 1) {
+            return false;
+        }
+
+        for (int i=2; i <= n/2; i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
+
+//    public static double calculateInterest(double amount, double interestRate) {
+//        return (amount * (interestRate / 100));
+//    }
 
 
 }
