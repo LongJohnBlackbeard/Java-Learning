5e2a8c78ba00541038be8a03ebfb0325fe8929b5
Daniel Tujo <tujo1992@gmail.com>
2021-02-18 20:16:38 -0600

    Initial Commit

---
+++ Notes/ReadingUserInput/src/Main.java
@@ -0,0 +1,23 @@
+// Reading User Input
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+        int yearOfBirth = scanner.nextInt();
+        int age = 2021 - yearOfBirth;
+        scanner.nextLine(); // handle next line character (enter key)
+
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+
+        scanner.close();
+
+    }
+}
